a0214ab864e097895586512a557602d564de134d
Jan_Niek <jantjedeeleveld@gmail.com>
2023-06-12 23:13:06 +0200

    wie weet

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const fontSize = window.getComputedStyle("body").fontSize;
 
-window.mobileCheck = function() {
-  if (navigator.userAgent.match(/Android/i)
+if (navigator.userAgent.match(/Android/i)
   || navigator.userAgent.match(/webOS/i)
   || navigator.userAgent.match(/iPhone/i)
   || navigator.userAgent.match(/iPad/i)
   || navigator.userAgent.match(/iPod/i)
   || navigator.userAgent.match(/BlackBerry/i)
-  || navigator.userAgent.match(/Windows Phone/i)) {
-    a = true;
-  } else {
-    a = false;
-  }
-  return a;
-}
-
-
-if (window.mobileCheck()){
-  fontSize = "xx-large";
-}
+  || navigator.userAgent.match(/Windows Phone/i)) 
+  {
+    fontSize = "xx-large";
+  } 
